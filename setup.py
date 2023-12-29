ecee803d819cea9108e213ea80edf7af936ea0aa
h-akira <79441679+h-akira@users.noreply.github.com>
2023-12-30 05:30:03 +0900

    new version

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@
 # Created: 2023-12-30 03:41:28
 
 from setuptools import setup, find_packages
-setup(name='ExchangeLibrary',version='1.0.0',packages=find_packages())
+setup(name='ExchangeLibrary',version='1.0.1',packages=find_packages())
