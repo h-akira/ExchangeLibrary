9b5c7bcd6dd388d88aa0b7b898540af9f799b230
h-akira <79441679+h-akira@users.noreply.github.com>
2023-12-30 05:33:24 +0900

    add

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@
 # Created: 2023-12-30 03:41:28
 
 from setuptools import setup, find_packages
-setup(name='ExchangeLibrary',version='1.0.1',packages=find_packages())
+setup(name='ExchangeLibrary',version='1.0.2', install_requires=_requires_from_file('requirements.txt'), packages=find_packages())
